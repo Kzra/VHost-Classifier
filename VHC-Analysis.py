2f1ad2f1e513bb1cde10c5e62dd4e076ac4d8037
Ezra Kitson <32281980+Kzra@users.noreply.github.com>
2019-01-15 15:35:12 +0000

    Added analysis script

---
+++ VHC-Analysis.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 28 14:57:54 2018
+
+@author: Ezra
+"""
+
+import os 
+import csv
+
+##One / write to column one, two // write to column two, three /// write to column three
+
+
+#os.chdir('Run7_270618/Virus/Host Assigned')
+cwd = os.getcwd()
+t1 = open('Total Counts.csv','w')
+tw =  csv.writer(t1,lineterminator='\n')
+
+col1 = []
+col2 = []
+col3 = []
+
+for root,dirs,files in os.walk('.'):
+    rc = root.count('\\')
+    if rc == 3:
+      col3.append([root,' '])
+    if rc == 2:
+      col2.append([root,' '])
+    if rc == 1:
+      col1.append([root,' '])
+    root1 = (cwd+'/'+ root)
+    os.chdir(cwd+'/'+root)
+    for f in files:
+        if f == 'COUNTS.csv': 
+            with open(f) as count:
+                counts = csv.reader(count)
+                for row in counts:
+                    if rc == 3:
+                        col3.append(row)
+                    if rc == 2:
+                        col2.append(row)
+                    if rc == 1:
+                        col1.append(row)
+                
+    os.chdir(cwd)
+    
+    
+lcol1 = len(col1)
+lcol2 = len(col2)
+lcol3 = len(col3)
+
+ml = max(lcol1,lcol2,lcol3)
+
+while len(col1) < ml:
+    col1.append([' ',' '])
+while len(col2) < ml:
+    col2.append(['  ',' '])
+while len(col3) < ml:
+    col3.append(['  ','  ']) 
+    
+for  i in zip(col1,col2,col3):
+          tw.writerow(i[0]+i[1]+i[2])
+
+t1.close()          
+
+
